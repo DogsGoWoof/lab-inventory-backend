d33cbe61e04b3c1212a0bc132e9d72f01af4abd0
jason <jason_tan75@hotmail.com>
2024-07-29 13:06:57 -0400

    synced reagents.js

--- controllers/reagents.js
+++ controllers/reagents.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 
 //======PROTECTED ROUTES ========
+router.use(verifyToken);
+
 router.post('/', async (req, res) => {
     try {
         req.body.author = req.user._id;
@@ -17,11 +19,11 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/', async (req, res) =>{
+router.get('/', async (req, res) => {
     try {
         const reagents = await Reagent.find({})
-        .populate('author')
-        .sort({ createdAt: 'desc' })
+            .populate('author')
+            .sort({ createdAt: 'desc' })
         res.status(200).json(reagents);
     } catch (error) {
         res.status(500).json(error);
@@ -29,7 +31,7 @@ router.get('/', async (req, res) =>{
 });
 
 router.get('/:reagentId', async (req, res) => {
-    try { 
+    try {
         const reagent = await Reagent.findById(req.params.reagentId).populate([
             'author',
             'comments.author',
@@ -37,9 +39,67 @@ router.get('/:reagentId', async (req, res) => {
 
         res.status(200).json(reagent);
     } catch (error) {
-      res.status(500).json(error);
+        res.status(500).json(error);
     }
 });
 
-router.use(verifyToken);
+router.put('/:reagentId', async (req, res) => {
+    try {
+        // Find the reagent:
+        const reagent = await Reagent.findById(req.params.reagentId);
+
+        // Check permissions:
+        if (!reagent.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+        // Update reagent:
+        const updatedReagent = await Reagent.findByIdAndUpdate(
+            req.params.reagentId,
+            req.body,
+            { new: true }
+        );
+
+        // Append req.user to the author property:
+        updatedReagent._doc.author = req.user;
+
+        // Issue JSON response:
+        res.status(200).json(updatedReagent);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.delete('/:reagentId', async (req, res) => {
+    try {
+        const reagent = await Reagent.findById(req.params.reagentId);
+
+        if (!reagent.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!");
+        }
+
+        const deletedReagent = await Reagent.findByIdAndDelete(req.params.reagentId);
+        res.status(200).json(deletedReagent);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post('/:reagentId/comments', async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const reagent = await Reagent.findById(req.params.reagentId);
+        reagent.comments.push(req.body);
+        await reagent.save();
+
+        //Find the newly created comment:
+        const newComment = reagent.comments[reagent.comments.length - 1];
+        newComment._doc.author = req.user;
+
+        res.status(201).json(newComment);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 module.exports = router;
